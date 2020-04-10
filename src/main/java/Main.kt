71325e10baaa529cd3b08ee7ee3b268858f2cd42
lumi saeki <eternal.rhythrm@gmail.com>
2020-04-11 03:58:47 +0900

    ua設定

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -31,7 +31,9 @@ fun main(args: Array<String>) {
     val cookieManager = CookieManager()
     cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL)
 
-    val tmp = "xxx"
+    val ua = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36"
+
+    val cookie = "xxx"
 
     // URLの指定
     val url = "https://vcard.ameba.jp/mypage"
@@ -42,8 +44,8 @@ fun main(args: Array<String>) {
     // リクエスト作成
     val request= Request.Builder()
             .url(url)
-            .header("Cookie", tmp)
-//            .addHeader("Accept", "tmp")
+            .header("User-Agent", ua)
+            .addHeader("Cookie", cookie)
             .get()
             .build()
 
